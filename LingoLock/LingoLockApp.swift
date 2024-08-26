455a32c53218cf2595ba7496dfa0ed4407821b62
perotins <maxfuligni@gmail.com>
2024-08-26 16:47:36 -0400

    Initial Commit

---
+++ LingoLock/LingoLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingoLockApp.swift
+//  LingoLock
+//
+//  Created by Max Fuligni on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LingoLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
